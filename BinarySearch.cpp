f241eb6adc50f91858bc077d53b10c1d5245631c
Raraandinii <raraandinib@gmail.com>
2025-05-07 13:42:52 +0700

    add bubbleSortArray function to sort input array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -25,5 +25,27 @@ void input()
     cout << "            Masukkan Elemen Array         \n";
     cout << "===========================================\n";
     for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
     
 }
